b0fcc0696a6156f0e2d0cc562e992803f68ef119
egautier <egautier@keywordsstudios.com>
2023-05-11 00:18:27 +0900

    Update script

--- script.sh
+++ script.sh
@@ -7,8 +7,8 @@ export PATH=$PATH:$DIR_SCRIPT
 # Define the URL of the page
 url="https://www.microsoft.com/en-us/download/confirmation.aspx?id=56519"
 
-# Create output and ranges-services-pa folders if they don't exist
-mkdir -p output/ranges-services-pa
+# Create ranges-services-pa folder if it doesn't exist
+mkdir -p ranges-services-pa
 
 # Download the HTML of the page
 page_html=$(curl -s "$url")
@@ -19,21 +19,26 @@ json_url=$(echo "$page_html" | grep -Po 'https://download.microsoft.com/download
 # Get the original filename from the URL
 json_filename=$(basename "$json_url")
 
-# Download the JSON file and save it to output folder with its original name
-curl -o "output/$json_filename" "$json_url"
+if [ -e "$json_filename" ]; then
+  echo "File $json_filename already exists. Exiting."
+  exit 1
+fi
+
+# Download the JSON file and save it to current directory with its original name
+curl -o "$json_filename" "$json_url"
 
 # Check if the downloaded file is a properly constructed JSON file
-jq empty "output/$json_filename" > /dev/null 2>&1
+jq empty "$json_filename" > /dev/null 2>&1
 if [ $? -ne 0 ]; then
   echo "Downloaded file is not a valid JSON. Exiting."
   exit 1
 fi
 
 # Make a copy of the file as ServiceTags_Public.json for processing
-cp "output/$json_filename" output/ServiceTags_Public.json
+cp "$json_filename" ServiceTags_Public.json
 
 # Get unique system services and convert it to an array
-SYSTEMS=($(jq -r '.values[].properties.systemService' output/ServiceTags_Public.json | sort -u))
+SYSTEMS=($(jq -r '.values[].properties.systemService' ServiceTags_Public.json | sort -u))
 
 # Calculate the total number of services
 total_services=${#SYSTEMS[@]}
@@ -53,20 +58,18 @@ for service in "${SYSTEMS[@]}"; do
   # If the service name is not empty, write the IP addresses to a new file named after the service in the output folder
   if [ -n "$service" ]; then
     # Clear the service file
-    > "output/ranges-services-pa/${service}.txt"
+    > "ranges-services-pa/${service}.txt"
 
     # Get all the addresses for this service
-    addresses=$(jq -r --arg service "$service" '.values[] | select(.properties.systemService == $service) | .properties.addressPrefixes[]?' output/ServiceTags_Public.json)
+    addresses=$(jq -r --arg service "$service" '.values[] | select(.properties.systemService == $service) | .properties.addressPrefixes[]?' ServiceTags_Public.json)
 
     # Loop through each address
     for address in $addresses; do
       # Print the address to the appropriate file
-      echo "$address" >> "output/ranges-services-pa/${service}.txt"
+      echo "$address" >> "ranges-services-pa/${service}.txt"
     done
   fi
 done
-
-cd output
 
 # Update the readme.md file with the source file information
 sed -i "/^# Last update.*/a This repository contains the IPs from \"$json_filename\"" readme.md
@@ -76,7 +79,7 @@ if [ ! -d ".git" ]; then
   git init
 fi
 
-# Add all files in the "output" directory to the Git repository
+# Add all files to the Git repository
 git add .
 
 # Commit the changes
@@ -85,6 +88,7 @@ git commit -m "Automated update"
 # Set the remote URL to your GitHub repository
 # Now we use the GITHUB_USERNAME and GITHUB_TOKEN variables
 git remote set-url origin https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/$GITHUB_USERNAME/azureIPranges.git
+git remote set-url origin https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/$GITHUB_USERNAME/$REPO_NAME
 
 # Push the changes to the GitHub repository
 git push -u origin main -f
